ca3e9119f4098112ef62e7ab7a3d087e3c29aa9d
zidau <zi.daugela@gmail.com>
2024-04-10 17:22:50 +0300

    Initial commit

---
+++ src/main/java/com/Zilvis/Mokslu/projektas/MoksluProjektasApplication.java
@@ -0,0 +1,13 @@
+package com.Zilvis.Mokslu.projektas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoksluProjektasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoksluProjektasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Zilvis/Mokslu/projektas/MoksluProjektasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zilvis.Mokslu.projektas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoksluProjektasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
